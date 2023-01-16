b7d227ef110ce8c72f08cd6c605ccc80857ffe05
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-16 14:52:17 +0600

    create dashboard api to myappointment

--- index.js
+++ index.js
@@ -23,11 +23,66 @@ async function run() {
     const appointmentOptionCollection = client
       .db("doctors-portal")
       .collection("appointmentOptions");
+    const bookingsCollection = client
+      .db("doctors-portal")
+      .collection("bookings");
+    const usersCollection = client.db("doctors-portal").collection("users");
 
     app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
+      const bookingQuery = { appointmentDate: date };
+      const allReadyBooked = await bookingsCollection
+        .find(bookingQuery)
+        .toArray();
+
+      options.forEach((option) => {
+        const optionBooked = allReadyBooked.filter(
+          (book) => book.treatment === option.name
+        );
+        const bookedSlots = optionBooked.map((book) => book.slot);
+        const remainingSlots = option.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        option.slots = remainingSlots;
+      });
+
       res.send(options);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
+    // Booking Post API
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment,
+      };
+
+      const allReadyBooked = await bookingsCollection.find(query).toArray();
+      if (allReadyBooked.length) {
+        const message = `You already have a book on ${booking.appointmentDate}`;
+        return res.send({ acknowledged: false, message });
+      }
+
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    //  Collect Users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     });
   } finally {
   }
